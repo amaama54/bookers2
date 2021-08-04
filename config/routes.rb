268502780b81f0fb382b26ada824c05969714273
EC2 Default User <ec2-user@ip-172-31-55-56.ec2.internal>
2021-08-04 05:31:25 +0000

    [add]apply4

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root "homes#top"
   get "home/about" => "homes#about"
-  resources :users, only: [:index, :show, :edit, :update, :create]
+  resources :users, only: [:index, :show, :edit, :update, :create] do
+    resource :relationships, only: [:create, :destroy]
+    get "followings" => "relationships#followings", as: "followings"
+    get "followers" => "relationships#followers", as: "followers"
+  end
   resources :books, only: [:index, :show, :edit, :update, :destroy, :create] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
