bf57184f539754fadafd97356b2834b9828fa931
EC2 Default User <centerfielder-msk-54@docomo.ne.jp>
2021-10-13 12:15:06 +0000

    [finish]js6

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  $("p").text("jQuery稼働テスト(稼働中)");
+  $("test").text("jQuery稼働テスト(稼働中)");
 });
 
 $(function(){
-  $('.box1').slideDown();
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
+  });
 });
