daf914e5d4ed62d314bb7aef674fa3665a09260d
EC2 Default User <centerfielder-msk-54@docomo.ne.jp>
2021-10-13 12:42:52 +0000

    [start]js7

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,10 +3,7 @@ $(document).ready(function() {
 });
 
 $(function(){
-  $('.box1').on('click',function(){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
+  $('.box1').on('click', function(){
+    $(this).slideUp();
   });
 });
