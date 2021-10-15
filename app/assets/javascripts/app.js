74435e590a63b633d16256da4c175beb94e4571b
EC2 Default User <centerfielder-msk-54@docomo.ne.jp>
2021-10-15 14:41:26 +0000

    [start]js13

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,24 +1,2 @@
-$(document).ready(function() {
-  $("test").text("jQuery稼働テスト(稼働中)");
-});
 
-$(function(){
-  $('.box1').on('click', function(){
-    $(this).slideUp();
-  });
-});
 
-$(function(){
-  $('button').on('click', function(){
-    $('ul').children().css('color', 'red');
-  });
-});
-
-$(function() {
-  $('#back a').on('click',function(event){
-    $('body, html').animate({
-      scrollTop:0
-    }, 800);
-    event.preventDefault();
-  });
-});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,33 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+  $("test").text("jQuery稼働テスト(稼働中)");
+});
+
+$(function(){
+  $('.box1').on('click', function(){
+    $(this).slideUp();
+  });
+});
+
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color', 'red');
+  });
+});
+
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
+  });
+});
+
+$(document).ready(function(){
+  $('.jquery').on('click', function(){
+    $(this).css('color', 'red');
+  });
+});
