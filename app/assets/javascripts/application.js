5972cd02fad3c02837c56573aa81ca00d5b44280
EC2 Default User <centerfielder-msk-54@docomo.ne.jp>
2021-10-16 01:28:20 +0000

    [finish]js11

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,4 +47,16 @@ document.addEventListener("turbolinks:load", function() {
       $(this).css('color', 'red');
     });
   });
+  
+  /*11章タブメニュー*/
+  $('#tab-contents .tab[id != "tab1"]').hide();
+  
+  $('#tab-menu a').on('click', function(event) {
+    $("#tab-contents .tab").hide();
+    $("#tab-menu .active").removeClass("active");
+    $(this).addClass("active");
+    $($(this).attr("href")).show();
+    event.preventDefault();
+  });
+  
 });
